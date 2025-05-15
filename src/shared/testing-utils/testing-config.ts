f5b9708ca204e1b26b2f04ca8e25d1ede76e1ead
Worfran <fw.garcia@uniandes.edu.co>
2025-05-15 13:06:25 -0500

    update: bump @nestjs/testing version to 11.1.1 and add TypeOrmTestingConfig utility

---
+++ src/shared/testing-utils/testing-config.ts
@@ -0,0 +1,18 @@
+/* archivo src/shared/testing-utils/typeorm-testing-config.ts*/
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+export const TypeOrmTestingConfig = () => [
+  TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: ':memory:',
+    dropSchema: true,
+    entities: [
+
+    ],
+    synchronize: true,
+  }),
+  TypeOrmModule.forFeature([
+
+  ]),
+];
